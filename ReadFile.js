f9e3c50e31012f366390e03c352f9173959a7280
ouarzeddine-aymen <159270957+ouarzeddine-aymen@users.noreply.github.com>
2024-11-02 22:36:15 +0100

    Delete ReadFile.js

--- ReadFile.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const filePath = process.argv[2];
-fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-        console.error("Error :", err);
-        return;
-    }
-    console.log(data);
-});
